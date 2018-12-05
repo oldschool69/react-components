545aecd61b96091003ecb399066522e9b1f729c4
Flavio Oliveira <flavio.m@samsung.com>
2018-12-05 15:32:41 -0200

    Using nested ui components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
-
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
-    </div>
-  );
+	return (
+		<div className="ui container comments">
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					timeAgo="Today at 4:45PM"
+					avatarImage={faker.image.avatar()}
+					blogPost="Hello!"
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Alex"
+					timeAgo="Today at 2:00PM"
+					avatarImage={faker.image.avatar()}
+					blogPost="Ferinha??"
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Jane"
+					timeAgo="Yesterday at 5:00PM"
+					avatarImage={faker.image.avatar()}
+					blogPost="Nice"
+				/>
+			</ApprovalCard>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
